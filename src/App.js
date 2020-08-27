c2167b36090757a1970baad22d7aa6de4c724a4e
herrmannluiza <herrmannluiza@gmail.com>
2020-08-27 10:16:13 -0300

    app ready

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() =>{
+    api.get('repositories').then(response => {
+      setRepositories(response.data) 
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: `Novo projeto ${Date.now()}`,
+      url: 'github.com/herrmannluiza',
+      techs: ['ReactJS', 'TypeScript']
+    });
+
+    setRepositories([ ...repositories, response.data ]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+
+    setRepositories(repositories.filter(
+      repository => repository.id !== id
+    ));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+      {/*Listar os repositórios da sua API: 
+          Deve ser capaz de criar uma lista com o campo title de todos os repositórios 
+          que estão cadastrados na sua API.*/}
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>Remover</button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
